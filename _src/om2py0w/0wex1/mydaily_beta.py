8023ea7db9a5c68f8a07e5c8b7982a547ae98405
xpgeng <xpgeng@126.com>
2015-10-21 20:41:41 +0800

    modify mydaily.py and README. md

--- _src/om2py0w/0wex1/mydaily_beta.py
+++ _src/om2py0w/0wex1/mydaily_beta.py
@@ -32,17 +32,3 @@ f.close()
 
 
 
-
-
-
-
-#birth = date(1989, 10, 29)
-#now = date.today()
-
-#now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
-
-#age = now - birth
-
-#print age.days
-
-
