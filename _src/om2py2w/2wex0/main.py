dcc26222b8002385df25d192ffb7cd82413c0929
xpgeng <xpgeng@126.com>
2015-10-30 01:14:19 +0800

    modify main.py and README.md

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -1,39 +1,60 @@
 # _*_ coding:utf-8 _*_
+import sys
 from Tkinter import *
 from time import gmtime, strftime
 
-class App(Frame):
+reload(sys)# 这是什么意思?
+sys.setdefaultencoding('utf-8')
+
+class Mydaily(Frame):
     
-    def __init__(self, master):
+    def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
 
-        self.entrythingy = Entry()
-        self.entrythingy.pack(padx=10, ipadx=300, pady=10)
+        self.entrything = Entry(master)
+        self.entrything.pack(ipadx=200, side=TOP)
         self.contents = StringVar()
-        self.entrythingy["textvariable"] = self.contents
-        self.entrythingy.bind('<Key-Return>',
-                              self.save_and_print_contents)
-        
-        self.showcontents = Label(textvariable=self.contents)
+        self.entrything["textvariable"] = self.contents
+                
+        #self.existcontents = StringVar()
+        self.showcontents = Text(height=20, width = 500)        
         self.showcontents.pack()
+        
+        self.entrything.bind('<Key-Return>',self.read_and_save)
 
         self.quit = Button(master, text="quit", command=self.cancel)
-        self.quit.bind()
-        self.quit.pack(pady=5)
+        self.quit.pack(pady=5, side=BOTTOM)
     
+    def  read_and_save(self, event):
+         self.read_contents(event)
+         self.save_and_print_contents(event)
+          
+
     def save_and_print_contents(self, event):
-        with open('diary.md','a') as f:
+        with open('diary.md','a') as self.f:
             now = strftime("%Y年%m月%d日 %H:%M:%S", gmtime())
-            f.write("%s\n\n %s\n\n" %(now, self.contents.get()))
-            f.close()
+            self.f.write("%s\n\n %s\n\n" %(now, self.contents.get()))
+            self.f.close()
         print "This is your input --->", self.contents.get()
+        self.entrything.delete(0,END)          #回车后,输入框清零.
+        #self.entrything.focus_set()             #什么用啊?
+
+    def read_contents(self, event):
+        lines = self.contents.get()
+        self.showcontents.insert(END, lines+'\n')
+        self.f.close()
 
     def cancel(self):
         self.master.destroy()
+    
 
+def main():
+    root = Tk()
+    mydaily = Mydaily(master=root)
+    mydaily.master.title('Mydaily')
+    mydaily.master.geometry('640x480')# size copy from @picklecai
+    mydaily.mainloop()
 
-root = Tk()
-app = App(master=root)
-app.mainloop()
-root.destroy()
+if __name__=="__main__":
+    main()
