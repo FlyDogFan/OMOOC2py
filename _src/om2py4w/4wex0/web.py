cb7ae937edb8de687f831629c459fd4264a9be5a
xpgeng <xpgeng@126.com>
2015-11-07 17:21:35 +0800

    add  save

--- _src/om2py4w/4wex0/web.py
+++ _src/om2py4w/4wex0/web.py
@@ -8,36 +8,45 @@ Mydaily-web-version-1.0
 """
 
 
-from bottle import *
-
+from bottle import * # 慎用, 会引入很多未知的东西, 或者全局变量神马的.
+from time import gmtime, strftime
+#from bottle import route, get, post, request, error, run
 
 #@route('/hello/<name>')
 #def index(name):
 #    return template('<b>Hello {{name}}</b>!', name=name)
 
-@route('/login')
-def login():
-	return '''
-	    <form action="/login" method="post">
-用户: <input name ="uname" type="text" />
-口令: <input name ="passw" type="password" />
-<input value ="Login" type="submit" />
-      </form>
-      '''	    
-@route('/login', method='POST')
-def do_login():
-	username = request.forms.get('uname')
-	password = request.forms.get('passw')
-	print "srv.got:", username, password
-	if username:
-		return template("<p>Done!<hr/>{{ name }}<p>"
-		         , name=username)	    
+@route('/mydaily')
+def mydaily():
+    return '''
+	    <form action="/mydaily" method="post">
+            日记: <input name ="content" type="text" />
+            <input value ="保存" type="submit" />
+        </form>
+    '''	 
+
+@route('/mydaily', method='POST')
+def save_mydaily():
+    daily_content = request.forms.get('content')
+    print "srv.got:", daily_content
+    if daily_content:
+        with open('diary.md','a') as f:
+            f.write("%s\n" % daily_content)
+            f.close()
+        return template("<p>Done!<hr/>{{ name }}<p>"
+        	, name= daily_content)
+		        	    
 
 
+
+
+#@error(404)# 404
+#def error404(error):
+#    return 'Nothing here, sorry'
 
 if __name__ == '__main__':
     debug(True)
-    run(host="localhost", port=8080, reloader=True)
+    run(host="localhost", port=8080, reloadeCautionr=True)
 
 
 
