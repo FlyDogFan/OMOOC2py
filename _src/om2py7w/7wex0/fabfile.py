8ec8b695ce2677a0de2beb6878d26c9eeeabb90a
xpgeng <xpgeng@126.com>
2015-11-30 14:20:00 +0800

    modifu fabfile.py

--- _src/om2py7w/7wex0/fabfile.py
+++ _src/om2py7w/7wex0/fabfile.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from fabric.api import env, run, local, settings
+from datetime import datetime
 
-def git():
-	local('pwd'
+def git(m='This guy left nothing to decribe this commit!'):
+	'''git:m = "COMMIT LOGGING"\t(default as 'This guy left 
+		        nothing to decribe this commit!')
+	'''
+	now = datetime.today()
+	local('pwd'		    
 		    '&& touch fab.txt'
-		    '&& echo "WOW" >> fab.txt'
-		    '&& date'
+		    '&& echo "{now}""\t{msg}" >> fab.txt'
+		    '&& git add .'
+		    '&& git commit -am "{msg}"'
+		    '&& git push'
+		    '&& date'.format(now = now, msg = m )
         )
 
 
