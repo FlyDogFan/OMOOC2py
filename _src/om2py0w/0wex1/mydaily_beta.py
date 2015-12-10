f74a2c78711869b15dda343babd7ba0d5d94eafb
xpgeng <xpgeng@126.com>
2015-12-10 18:01:24 +0800

    add two items to 7wd4.md

--- _src/om2py0w/0wex1/mydaily_beta.py
+++ _src/om2py0w/0wex1/mydaily_beta.py
@@ -12,23 +12,22 @@ def main():
 #get the commands of  quit, write or check the past
     choice_command = raw_input('Hey! buddy!\nq:quit\nw:write a new diadry\np:check the past diaries?\n> ')
 
-    if choice_command == "q": #here maybe can add several words
+    if choice_command in ['q','quit'] : #here maybe can add several words
 	    print 'See you later..'
 	    quit() 
 #write the diary
-    elif choice_command == "w":
+    elif choice_command == "w" :
 	    input_content = raw_input('Let\'s write: ')
 	    save_or_not = raw_input('Do you want to save this diary? y/n: ')
 	
-	    if save_or_not == "y":
+	    if save_or_not == ['y','Y', 'yes']:
 	        with open('diary.md','a') as f:
 	    	    now = strftime("%Y年%m月%d日 %H:%M:%S", gmtime())
 	            f.write("%s\n\n %s\n\n" %(now, input_content))
                 print "Yours diary has been saved."
 	    elif save_or_not == "n":
 		    print "Your diary has been deleted..."
-		    quit# here can add  write another one, maybe will use some modules
-		    #or defs... cycle?
+		    quit()
 #show all the past diaries
     elif choice_command == "p":
         with open('diary.md','r') as f:
